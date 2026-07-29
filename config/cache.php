ceedc616c548b6270e101f81c16dc96611ebd520
pratikmalani <malanipratik9@gmail.com>
2026-07-29 12:44:57 +0530

    Fixed session and database configuration

--- config/cache.php
+++ config/cache.php
@@ -15,8 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
-
+    'default' => env('CACHE_STORE', 'file'),
     /*
     |--------------------------------------------------------------------------
     | Cache Stores
@@ -118,7 +117,7 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
+    'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-cache-'),
 
     /*
     |--------------------------------------------------------------------------
